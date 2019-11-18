c09a7ef4d427401a73615bb789e05f05a6f78517
kiladzeandrei <kiladze.a@pm.by>
2019-11-18 15:06:08 +0300

    json

--- js.js
+++ js.js
@@ -1,23 +1,28 @@
-const fs = require('fs'), path = require('path'), filePath = path.join(__dirname, 'in.csv');
+const fs = require('fs'), path = require('path'),
+    inPath = path.join(__dirname, 'in.csv'),
+    outPath = path.join(__dirname, 'out.json');
 
 const count = (a,b) => {
     let k=0;
     for (let i=0;i<a.length;i++) {
-        for (let j=0;j<b.length;j++) {
-            if (a[i]===b[j]) {
-                k=k+1;
-            }
+        if (b.includes(a[i])) {
+            k=k+1;
         }
     }
     return k;
 };
 
-let raw = fs.readFileSync(filePath,'utf8');
-raw = raw.split('\n').map(a=> a.split(',').map(b=> parseInt(b,10))).reverse().slice(1);
+let raw = fs.readFileSync(inPath,'utf8');
+raw = raw.split('\n').map(a=> a.split(',').map(b=> parseInt(b,10))).reverse().slice(1,200);
 let l=raw.length;
 let calc=[];
 for (let i=0;i<l;i++) {
-    calc.push(raw.map(a => count(raw[i],a)));
+    let row = {d:raw[i], m:[]};
+    for (let k=i+1;k<l;k++) {
+        row.m.push(count(raw[i],raw[k]));
+    }
+    calc.push(row);
 }
-console.log(calc[0]);
+let f= fs.createWriteStream(outPath);
+f.write(JSON.stringify(calc));
 
